23cc23332f0ad667b9ffe445cab31da4264361ba
neeharikasingh <neeharika.singh29@gmail.com>
2016-07-24 01:55:59 -0700

    update hostname/ip and port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 8080;
+const hostname = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
